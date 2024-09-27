9b7910da0047ea6e4ff4838033524ab319a026ee
ZwerOxotnik <zweroxotnik@gmail.com>
2024-09-28 00:47:57 +0300

    Update to 0.7.0

--- models/reclaiming_system.lua
+++ models/reclaiming_system.lua
@@ -5,19 +5,56 @@ local M = {}
 
 --#region Global data
 --local _players_data
+local on_pre_entity_force_changed
+local on_entity_force_changed
 --#endregion
 
 
 --#region Constants
---local ABS = math.abs
+local call = remote.call
 --#endregion
+
+
+local is_on_pre_entity_force_changed_event_active = false
+local is_on_entity_force_changed_event_active     = false
+if script.active_mods.EasyAPI then
+	-- Finds settings from other mod config files
+	for mod_name in pairs(script.active_mods) do
+		local is_ok, config = pcall(require, string.format("__%s__/external_mod_configs", mod_name))
+		if is_ok then
+			if config.is_on_pre_entity_force_changed_event_active then
+				is_on_pre_entity_force_changed_event_active = true
+				if is_on_entity_force_changed_event_active then
+					break
+				end
+			end
+			if config.is_on_entity_force_changed_event_active then
+				is_on_entity_force_changed_event_active = true
+				if is_on_pre_entity_force_changed_event_active then
+					break
+				end
+			end
+		end
+	end
+end
 
 
 --#region Pre-game stage
 
 local function link_data()
 	--_players_data = global.players
+
+	if script.active_mods.EasyAPI then
+		on_pre_entity_force_changed = call("EasyAPI", "get_event_name", "on_pre_entity_force_changed")
+		on_entity_force_changed     = call("EasyAPI", "get_event_name", "on_entity_force_changed")
+	end
 end
+
+
+local function get_data()
+
+end
+
 
 local function update_global_data()
 	--global.players = global.players or {}
@@ -75,9 +112,62 @@ local function reclaim_command(cmd)
 		radius = settings.global["recl_reclaiming_distance"].value,
 		force = enemy_forces
 	}
-	for _, entity in pairs(entites_for_capturing) do
-		-- is it safe?
-		entity.force = player_force
+	local raise_event = script.raise_event
+	-- TODO: ask if is allowed to change force from other mods
+	if not is_on_pre_entity_force_changed_event_active and
+		not is_on_entity_force_changed_event_active
+	then
+		for _, entity in pairs(entites_for_capturing) do
+			entity.force = player_force
+		end
+	elseif is_on_pre_entity_force_changed_event_active and
+		not is_on_entity_force_changed_event_active
+	then
+		local pre_event_data = {
+			entity = nil,
+			next_force = player_force
+		}
+		for _, entity in pairs(entites_for_capturing) do
+			pre_event_data.entity = entity
+			raise_event(on_pre_entity_force_changed, pre_event_data)
+			if entity.valid then
+				entity.force = player_force
+			end
+		end
+	elseif not is_on_pre_entity_force_changed_event_active and
+		is_on_entity_force_changed_event_active
+	then
+		local event_data = {
+			entity = nil,
+			next_force = player_force
+		}
+		for _, entity in pairs(entites_for_capturing) do
+			event_data.entity = entity
+			event_data.prev_force = entity.force
+			entity.force = player_force
+			raise_event(on_entity_force_changed, event_data)
+		end
+	elseif is_on_pre_entity_force_changed_event_active and
+		is_on_entity_force_changed_event_active
+	then
+		local pre_event_data = {
+			entity = nil,
+			next_force = nil
+		}
+		local event_data = {
+			entity = nil,
+			prev_force = nil
+		}
+		for _, entity in pairs(entites_for_capturing) do
+			pre_event_data.entity = entity
+			raise_event(on_pre_entity_force_changed, pre_event_data)
+			if entity.valid then
+				event_data.entity = entity
+				event_data.prev_force = entity.force
+				entity.force = player_force
+				raise_event(on_entity_force_changed, event_data)
+			end
+		end
 	end
 end
 
